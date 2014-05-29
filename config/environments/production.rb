911dc1362e61f0550322d8cff161ece147972978
Margie Roswell <mroswell@gmail.com>
2014-05-29 09:20:21 -0400

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+    # http://stackflow.herokuapp.com
+  config.action_mailer.default_url_options = { host: 'stackflow.herokuapp.com' }
 end
