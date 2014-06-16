4ccfc4abec0eb091e4d12dbdd508fcdde2e0fe92
Margie Roswell <mroswell@gmail.com>
2014-06-15 21:01:31 -0400

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Bloccit::Application.routes.draw do
   devise_for :users
-  resources :users, only: [:update]
+  # resources :users, only: [:update]
+  resources :users, only: [:show, :index, :update]
+
   resources :topics do
-    resources :posts
+    resources :posts, except: [:index], controller: 'topics/posts' do
+      resources :comments, only: [:create]
     #, except: [:index]
+    end
   end
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
